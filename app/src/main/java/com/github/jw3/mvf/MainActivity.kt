aa40b31ff1d2f56268abb222447d5525cad18e48
John Wass <jwass3@gmail.com>
2019-09-21 15:10:26 -0400

    load basemap from external storage

--- app/src/main/java/com/github/jw3/mvf/MainActivity.kt
+++ app/src/main/java/com/github/jw3/mvf/MainActivity.kt
@@ -1,10 +1,15 @@
 package com.github.jw3.mvf
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Environment
+import androidx.appcompat.app.AppCompatActivity
+import com.esri.arcgisruntime.layers.RasterLayer
 import com.esri.arcgisruntime.mapping.ArcGISMap
 import com.esri.arcgisruntime.mapping.Basemap
+import com.esri.arcgisruntime.mapping.Viewpoint
+import com.esri.arcgisruntime.raster.Raster
 import kotlinx.android.synthetic.main.activity_main.*
+import java.nio.file.Paths
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,6 +17,18 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mapView.map = ArcGISMap(Basemap.Type.IMAGERY, 42.0, 42.0, 4)
+        val layers = Environment.getExternalStorageDirectory().list()
+            .filter { it.endsWith(".tif") }
+            .map { Paths.get(Environment.getExternalStorageDirectory().path, it).toString() }
+            .map { Raster(it) }
+            .map { RasterLayer(it) }
+
+        mapView.map = ArcGISMap(Basemap(layers, emptyList()))
+        mapView.map.initialViewpoint = Viewpoint(42.0, 42.0, 4.0)
+
+        mapView.map.addDoneLoadingListener {
+            println(mapView.map.spatialReference.wkid)
+            println(mapView.map.spatialReference.unit.name)
+        }
     }
 }
